a757713e78113ed28ff6a66479d464a2323f9d3d
leila-cruz <leila.cruz666@gmail.com>
2025-11-24 10:40:57 -0300

    Initial commit

---
+++ src/test/java/org/example/ExamenMercadoLibreGlobal/MutantDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ExamenMercadoLibreGlobal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
